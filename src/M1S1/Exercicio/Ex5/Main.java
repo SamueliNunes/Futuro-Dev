65dd95a2c47c152b6941d363d3b69c99483ec3f2
samuelikz <samuel.nds@outlook.com>
2022-11-27 20:21:15 -0300

    Sequência de fibonacci

--- src/M1S1/Exercicio/Ex5/Main.java
+++ src/M1S1/Exercicio/Ex5/Main.java
@@ -5,17 +5,17 @@ public class Main {
     public static void main(String[] args){
         int
                 repetir,
-                b = 0,
-                c = 1;
+                a = 0,
+                b = 1;
         Scanner ler = new Scanner(System.in);
 
         System.out.print("Fibonacci - Informe a quantidade de vezes que deseja verificar na sequecia: ");
         repetir = ler.nextInt();
 
         for(int i = 0; i < repetir; i++){
-            b = b + c;
-            c = b - c;
-            System.out.print("["+b+"],");
+            a = a + b;
+            b = a - b;
+            System.out.print("["+a+"],");
         }
     }
 }
