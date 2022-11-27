3f90034651d2fdc499e77a9172f0b81af9ac6c00
samuelikz <samuel.nds@outlook.com>
2022-11-27 18:35:25 -0300

    sistemas de venda ex4

--- src/M1S1/Exercicio/Ex5/Main.java
+++ src/M1S1/Exercicio/Ex5/Main.java
@@ -1,6 +1,16 @@
 package M1S1.Exercicio.Ex5;
-import java.util.ArrayList;
 
 public class Main {
+    public static void main(String[] args){
+        int num1 = 2, num2 = 3;
 
+        System.out.println(num2);
+        System.out.println(num1);
+
+        for(int i = 0; i < 8; i++){
+            num1 = num1 + num2;
+            num2 = num1 - num2;
+            System.out.println(num1);
+        }
+    }
 }
---
+++ src/M1S1/Exercicio/Ex6/Main.java
@@ -0,0 +1,20 @@
+package M1S1.Exercicio.Ex6;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args){
+        String[] pessoas = {"Ana","Roberto","Samuel","Marcos","Maria","Paloma"};
+        Scanner ler = new Scanner(System.in);
+        int select;
+
+        do{
+            System.out.print("Escolha seu amigo secreto (números de 1 à 6):");
+            select = ler.nextInt();
+
+            if(select <= 0 || select >=7){
+                System.out.println("Numero Invalido");
+            }
+        }while (select <=0 || select >= 7);
+        System.out.println("Seu amigo secreto é: " + pessoas[select-1]);
+    }
+}
